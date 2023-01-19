259019e33c5fd752a5d947a8a18710372ba50f01
Jim <jinchglin@gmail.com>
2023-01-19 22:22:12 +0800

    add configuration

--- app/main.go
+++ app/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"lineChess/routes"
+	"lineChess/conf"
 	"log"
 )
 func main() {
+	config := conf.Conf()
 	engine := routes.New()
-	log.Fatal(engine.Run())
+	log.Fatal(engine.Run(config.URL))
 }
