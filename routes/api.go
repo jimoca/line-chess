2594e756794f5d5537b83c6cb02eef095a76cd21
Jim <jinchglin@gmail.com>
2023-01-19 22:22:33 +0800

    feat: web socket test

--- routes/api.go
+++ routes/api.go
@@ -5,6 +5,10 @@ import (
 )
 
 func init() {
+	socket := engine.Group("/socket")
+	{
+		socket.GET("", controllers.NewGame)
+	}
 	unAuthApi := engine.Group("api")
 	{
 		unAuthApi.GET("/hello", controllers.Hello)
